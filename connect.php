cef8e983a82a053cda31ffb2bcb6532092a3d256
Uprzejmy <lmerynda@gmail.com>
2016-11-02 12:13:25 +0100

    small fix

--- connect.php
+++ connect.php
@@ -1,6 +1,15 @@
 <?php
   include("parameters.php");
   
-  $mysqli = new mysqli($dbHost, $dbUser, $dbPassword, $dbName)
-    or die("There is no connection to MySQL server.\r\n");
+  $mysqli = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
+
+  if($mysqli->connect_errno)
+  {
+  	die("Failed to connect to database server.\r\n");
+  }
+  else
+  {
+  	echo "Connected to database server.\r\n";
+  }
+    
 ?>
