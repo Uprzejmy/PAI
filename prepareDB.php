fde9f618c0b858516533bd7899441fa0d2d9165e
Uprzejmy <lmerynda@gmail.com>
2016-11-03 01:04:17 +0100

    database (teams in matches) upgraded, some creation triggers added

--- prepareDB.php
+++ prepareDB.php
@@ -23,15 +23,17 @@
   $mysqli->close();
   include("connect.php");
 
-  $queries = array
+  $tablesCreationQueries = array
   (
     'CREATE TABLE users (
       id INT AUTO_INCREMENT NOT NULL,
       email VARCHAR(127) NOT NULL, 
-      password VARCHAR(127) NOT NULL, 
+      password VARCHAR(127) NOT NULL,
+      salt VARCHAR(127) NOT NULL,
       surname VARCHAR(127) NOT NULL,
       name VARCHAR(127) NOT NULL,
       session_key VARCHAR(127),
+      registered_at DATETIME,
       PRIMARY KEY(id),
       UNIQUE KEY email (email)     
     ) 
@@ -45,22 +47,24 @@
       name VARCHAR(127) NOT NULL,
       tag VARCHAR(15) NOT NULL,
       description VARCHAR(2000),
+      created_at DATETIME,
       PRIMARY KEY(id),
       UNIQUE KEY leader_id (leader_id),
-      CONSTRAINT team_leader_id FOREIGN KEY (leader_id) REFERENCES users (id)
+      CONSTRAINT teams_leader_id FOREIGN KEY (leader_id) REFERENCES users (id)
     )
     DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci 
     ENGINE = InnoDB    
     ',
 
-    'CREATE TABLE users_teams (
+    'CREATE TABLE teams_members (
       id INT AUTO_INCREMENT NOT NULL,
       user_id INT NOT NULL,
       team_id INT NOT NULL,
       joined_date DATETIME NOT NULL,
+      status VARCHAR(15) NOT NULL,
       PRIMARY KEY(id),
-      CONSTRAINT users_teams_user_id FOREIGN KEY (user_id) REFERENCES users (id),
-      CONSTRAINT users_teams_team_id FOREIGN KEY (team_id) REFERENCES teams (id)
+      CONSTRAINT teams_members_user_id FOREIGN KEY (user_id) REFERENCES users (id),
+      CONSTRAINT teams_members_team_id FOREIGN KEY (team_id) REFERENCES teams (id)
     )
     DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci 
     ENGINE = InnoDB    
@@ -71,6 +75,7 @@
       admin_id INT NOT NULL,
       name VARCHAR(127) NOT NULL,
       description VARCHAR(2000),
+      created_at DATETIME,
       PRIMARY KEY(id),
       CONSTRAINT tournaments_users_id FOREIGN KEY (admin_id) REFERENCES users (id)
     )
@@ -94,20 +99,61 @@
     'CREATE TABLE matches (
       id INT AUTO_INCREMENT NOT NULL,
       tournament_id INT NOT NULL,
-      team1_id INT NOT NULL,
-      team2_id INT NOT NULL,
-      joined_date DATETIME NOT NULL,
+      match_date DATETIME NOT NULL,
       PRIMARY KEY(id),
-      CONSTRAINT matches_tournaments_tournament_id FOREIGN KEY (tournament_id) REFERENCES tournaments (id),
-      CONSTRAINT matches_teams_team1_id FOREIGN KEY (team1_id) REFERENCES teams (id),
-      CONSTRAINT matches_teams_team2_id FOREIGN KEY (team2_id) REFERENCES teams (id)
+      CONSTRAINT matches_tournaments_tournament_id FOREIGN KEY (tournament_id) REFERENCES tournaments (id)
+    )
+    DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci 
+    ENGINE = InnoDB    
+    ',
+
+    'CREATE TABLE teams_matches (
+      id INT AUTO_INCREMENT NOT NULL,
+      match_id INT NOT NULL,
+      team_id INT NOT NULL,
+      scores INT,
+      PRIMARY KEY(id),
+      CONSTRAINT teams_matches_match_id FOREIGN KEY (match_id) REFERENCES matches (id),
+      CONSTRAINT teams_matches_team_id FOREIGN KEY (team_id) REFERENCES teams (id)
     )
     DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci 
     ENGINE = InnoDB    
     ',
   );
 
-  foreach($queries as $key => $query)
+  foreach($tablesCreationQueries as $key => $query)
+  {
+    $mysqli->query($query);
+
+    if($mysqli->connect_errno)
+    {
+      die("Failed to connect to database server.\r\n");
+    }
+  }
+
+  //triggers
+  $triggersCreationQueries = array
+  (
+    'CREATE TRIGGER tournament_creation 
+      BEFORE INSERT ON tournaments 
+      FOR EACH ROW
+        SET NEW.created_at = NOW();
+    ',
+
+    'CREATE TRIGGER teams_creation 
+      BEFORE INSERT ON teams 
+      FOR EACH ROW
+        SET NEW.created_at = NOW();
+    ',
+
+    'CREATE TRIGGER users_registration
+      BEFORE INSERT ON users 
+      FOR EACH ROW
+        SET NEW.registered_at = NOW();
+    ',
+  );
+
+  foreach($triggersCreationQueries as $key => $query)
   {
     $mysqli->query($query);
 
