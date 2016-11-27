06bbc34df4574b8cdeb44cd0140849e4bf5d2f3b
Uprzejmy <lmerynda@gmail.com>
2016-11-27 16:22:31 +0100

    custom not found page added

---
+++ notfound.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>404 - not found</title>
+  </head>
+  <body>
+    <a href="index.php">Homepage</a>
+    <a href="login.php">Login</a>
+    <a href="registration.php">Registration</a>
+    <p>Nie odnaleziono strony!</p>
+  </body>
+</html>
