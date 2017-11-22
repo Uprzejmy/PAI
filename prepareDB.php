33e72a17c0938509cea27cefb94f8145c076e345
Uprzejmy <lmerynda@gmail.com>
2017-11-22 20:51:44 +0100

    php database schema import

--- prepareDB.php
+++ prepareDB.php
@@ -23,6 +23,37 @@
   $mysqli->close();
   include("connect.php");
 
+  echo "Tables import started..";
+
+  // source: https://stackoverflow.com/questions/19751354/how-to-import-sql-file-in-mysql-database-using-php
+  // Name of the file
+  $filename = 'pai.sql';
+
+  // Temporary variable, used to store current query
+  $templine = '';
+  // Read in entire file
+  $lines = file($filename);
+  // Loop through each line
+  foreach ($lines as $line)
+  {
+    // Skip it if it's a comment
+    if (substr($line, 0, 2) == '--' || $line == '')
+        continue;
+
+    // Add this line to the current segment
+    $templine .= $line;
+    // If it has a semicolon at the end, it's the end of the query
+    if (substr(trim($line), -1, 1) == ';')
+    {
+        // Perform the query
+        $mysqli->query($templine) or print('Error performing query: ' . $templine . '<br /><br />');
+        // Reset temp variable to empty
+        $templine = '';
+    }
+  }
+ echo "Tables imported successfully";
+?>
+  /*
   $tablesCreationQueries = array
   (
     'CREATE TABLE users (
@@ -192,5 +223,6 @@
     echo("Can't get user info\r\n");
     die();
   }
-  
+
 ?>
+*/
