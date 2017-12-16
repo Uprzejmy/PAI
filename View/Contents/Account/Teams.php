b585c863bc07567ecb66c0d8bdc433fa4f3a8a55
Uprzejmy <lmerynda@gmail.com>
2017-12-16 22:55:54 +0100

    links in account teams view are now clickable at any place

--- View/Contents/Account/Teams.php
+++ View/Contents/Account/Teams.php
@@ -9,16 +9,31 @@
             </li>
         </ul>
     </div>
-    <div class="content_list">
-        <ul>
-            <?php
+    <div class="content_below">
+        <div class="content_list">
+            <ul>
+                <?php
                 foreach($parameters['teams'] as $team)
                 {
                     $id = $team->getId();
                     $name = $team->getName();
-                    echo("<li>$name</li>");
+                    echo("<li><a href='/teams/$id'>$name</a></li>");
                 }
-            ?>
-        </ul>
+                ?>
+            </ul>
+        </div>
+        <div class="content_actions">
+            <ul>
+              <?php
+              echo "<li>actions</li>";
+              //          foreach($parameters['teams'] as $team)
+              //          {
+              //            $id = $team->getId();
+              //            $name = $team->getName();
+              //            echo("<li>$name</li>");
+              //          }
+              ?>
+            </ul>
+        </div>
     </div>
 </div>
